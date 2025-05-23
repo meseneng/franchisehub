53f0a83d3fea6e836bee0f1be6999ea6af1c8230
meseneng <mesebeng17@gmail.com>
2025-05-24 02:32:45 +0800

    Add pages/index.tsx homepage

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 50 }}>
+      <h1>Halo dari FranchiseHub!</h1>
+      <p>Ini halaman utama sederhana. Siap online!</p>
+    </main>
+  )
+}
